169aa910c124d2b7cd678af68a0da9fb9fc363e6
Jay <54886938+JayXTQ@users.noreply.github.com>
2023-01-18 06:02:47 -0800

    added image reading

--- src/commands/auto-response/create.js
+++ src/commands/auto-response/create.js
@@ -5,24 +5,51 @@ module.exports = {
     data: new SlashCommandBuilder()
         .setName("create")
         .setDescription("Creates an autoresponse")
-        .addStringOption(option =>
-            option.setName("pickups")
-                .setDescription("Comma-seperated list of words that will trigger the output")
-                .setRequired(true)
-        )
-        .addStringOption(option => 
-            option.setName("output")
-                .setDescription("What do you want to be said in the output?")
-                .setRequired(true)
-        )
-        .addChannelOption(option => 
-            option.setName("channel")
-                .setDescription("What channel do you want this to work in?")
-                .setRequired(true)
-        )
         .setDefaultMemberPermissions(PermissionFlagsBits.ManageMessages)
-        .setDMPermission(false),
+        .setDMPermission(false)
+        .addSubcommand(subcommand =>
+            subcommand
+                .setName("text")
+                .setDescription("For creating a text based auto-response")
+                .addStringOption(option =>
+                    option.setName("pickups")
+                        .setDescription("Comma-seperated list of words that will trigger the output")
+                        .setRequired(true)
+                )
+                .addStringOption(option => 
+                    option.setName("output")
+                        .setDescription("What do you want to be said in the output?")
+                        .setRequired(true)
+                )
+                .addChannelOption(option => 
+                    option.setName("channel")
+                        .setDescription("What channel do you want this to work in?")
+                        .setRequired(true)
+                )
+        )
+        .addSubcommand(subcommand => 
+            subcommand
+                .setName("image")
+                .setDescription("For creating an image based auto-response")
+                .addStringOption(option =>
+                    option.setName("pickups")
+                        .setDescription("Comma-seperated list of words that will trigger the output")
+                        .setRequired(true)
+                )
+                .addStringOption(option => 
+                    option.setName("output")
+                        .setDescription("What do you want to be said in the output?")
+                        .setRequired(true)
+                )
+                .addChannelOption(option => 
+                    option.setName("channel")
+                        .setDescription("What channel do you want this to work in?")
+                        .setRequired(true)
+                )
+        ),
     async execute(interaction, client) {
+        if(!interaction.options.getSubcommand()) return interaction.reply({ "content": "You need to enter a subcommand" })
+
         const db = new QuickDB()
 
         let pickups = interaction.options.getString("pickups");
@@ -45,7 +72,8 @@ module.exports = {
         await db.push("autoResponseData", {
             pickups: pickups,
             output: output,
-            channel: channel
+            channel: channel,
+            type: interaction.options.getSubcommand()
         });
 
         let aRDlen = await db.get("autoResponseData");
--- src/events/client/messageCreate.js
+++ src/events/client/messageCreate.js
@@ -1,4 +1,5 @@
 const { QuickDB } = require("quick.db");
+const Tesseract = require("tesseract.js");
 
 module.exports = {
   name: "messageCreate",
@@ -8,10 +9,30 @@ module.exports = {
     for (const response of aRD) {
       if(message.channelId != response.channel.id) return;
       let howmanyin = 0;
-      for (const pickup of response.pickups) {
-        if(message.content.includes(pickup)) howmanyin++;
+      switch(response.type){
+        case "text":
+          for (const pickup of response.pickups) {
+            if(message.content.includes(pickup)) howmanyin++;
+          }
+        case "image":
+          if(message.attachments.size > 0){
+            if (message.attachments.every(attachIsImage)){
+              for (const { url } of message.attachments.toJSON()){
+                const { data: { text } } = await Tesseract.recognize(url, client.config.lang);
+                for (const pickup of response.pickups) {
+                  if(howmanyin === (response.pickups.length)) break;
+                  if(text.includes(pickup)) howmanyin++;
+                }
+              }
+            }
+          }
       }
       if(howmanyin === (response.pickups.length)) message.channel.send(response.output)
     }
   },
 };
+
+function attachIsImage(msgAttach) {
+  var url = msgAttach.url;
+  return url.indexOf("png", url.length - "png".length) !== -1;
+}
